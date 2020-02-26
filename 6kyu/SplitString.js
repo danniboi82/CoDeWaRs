c0f99e4c011b7619b4ca1ab9b42be35b38598e58
danniboi82 <danoh82@gmail.com>
2020-02-26 14:46:32 -0800

    adding 6kyu kata

---
+++ 6kyu/SplitString.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it splits the string into pairs of two characters. 
+// If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// solution('abc') // should return ['ab', 'c_']
+// solution('abcdef') // should return ['ab', 'cd', 'ef']
+
+//Naive Solution
+function solution(str){
+    return str.split(/([a-z][a-z])/gi)
+        .filter(Boolean)
+            .map(char => char.length % 2 === 0 ? char : `${char}_`);
+ }
