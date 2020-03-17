777d117c0b1692a3a9495ff96ecd2994feba9362
danniboi82 <danoh82@gmail.com>
2020-03-17 14:33:08 -0700

    adding kata

---
+++ 7kyu/RegexValidatePinCode.js
@@ -0,0 +1,20 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but 
+// exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// eg:
+
+
+
+//Naive Solution 
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/gi.test(pin);
+}
+
+console.log(validatePIN("1234"))
+console.log(validatePIN("12345"))
+console.log(validatePIN("a234"))
+ //=== true
+ //=== false
+ //=== false
